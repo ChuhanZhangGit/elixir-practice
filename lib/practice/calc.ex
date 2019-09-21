b5ccd8b7dc2685545376361b062ba97be2ddac6a
Chuhan <zhangchuhan1995@gmail.com>
2019-09-21 16:48:11 -0400

    evaluate expr

--- lib/practice/calc.ex
+++ lib/practice/calc.ex
@@ -4,29 +4,52 @@ defmodule Practice.Calc do
     num
   end
 
+
   # def tag_tokens(expr) do
   #     map filter reduce foreach 
   # end
 
+  def checkExpr(expr) do 
+    String.match?(expr, ~r/^[[\d][\+|\-|\*|\/][\d]]*|[[\d]]$/)
+  end
 
+  def evaluate(a, b, op) do 
+    case op do
+      "+" -> a + b
+      "-" -> a - b
+      "*" -> a * b
+      "/" -> a/b
+      _-> raise ArgumentError, message: "invalid operator"   
+    end
+  end
 
   def calc(expr) do
     # This should handle +,-,*,/ with order of operations,
     # but doesn't need to handle parens.
     expr
-    Code.eval_string(expr)
-    |> elem(0)
-    # |> String.split(~r/\s+/)
-    # |> hd
-    # |> parse_float
-    # |> :math.sqrt()
+    nospace= String.replace(expr, ~r" ", "")
+    if checkExpr(nospace) == true do 
+      numList = String.split(nospace, ~r/\+|\-|\*|\//) 
+      |> Enum.filter(fn num -> num != "" end)
+      operatorList = String.split(nospace, ~r/\d/) 
+      |> Enum.filter(fn operator -> operator != "" end)
+      IO.puts(inspect(numList))
+      IO.puts(inspect(operatorList))
+      Enum.reduce(numList, fn ele, acc -> [head | tail] = operatorList
+        evaluate(elem, acc, head) operatorList = tail end)
+  
+    else
+      "Invalid expression"
+    end
 
-    # Hint:
+  end
+end
+
+
+      # Hint:
     # expr
     # |> split
     # |> tag_tokens  (e.g. [+, 1] => [{:op, "+"}, {:num, 1.0}]
     # |> convert to postfix
     # |> reverse to prefix
     # |> evaluate as a stack calculator using pattern matching
-  end
-end
