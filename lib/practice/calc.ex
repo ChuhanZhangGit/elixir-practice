12a6340816fecf3507004777658deda881f009a7
Chuhan <zhangchuhan1995@gmail.com>
2019-09-22 21:52:21 -0400

    edit calc

--- lib/practice/calc.ex
+++ lib/practice/calc.ex
@@ -1,19 +1,11 @@
 defmodule Practice.Calc do
-  def parse_float(text) do
-    {num, _} = Float.parse(text)
-    num
-  end
-
-
-  # def tag_tokens(expr) do
-  #     map filter reduce foreach 
-  # end
 
   def checkExpr(expr) do 
-    String.match?(expr, ~r/^[[\d][\+|\-|\*|\/][\d]]*|[[\d]]$/)
+    String.match?(expr, ~r/^[[\d][\+|\-|\*|\/][\d]]*$|^[[\d]]$/)
   end
 
-  def evaluate(a, b, op) do 
+  def evaluate(a, b, op) do
+
     case op do
       "+" -> a + b
       "-" -> a - b
@@ -23,21 +15,32 @@ defmodule Practice.Calc do
     end
   end
 
+  def recursive(numList, operatorList, result) do
+    if length(numList) == 0 do
+      result
+    else 
+      [secondNum | numListLeft] = numList
+      [operator | operatorListLeft] = operatorList
+      a = evaluate(result, secondNum, operator)
+      recursive(numListLeft, operatorListLeft, a)
+    end
+  end 
+
   def calc(expr) do
     # This should handle +,-,*,/ with order of operations,
     # but doesn't need to handle parens.
-    expr
     nospace= String.replace(expr, ~r" ", "")
     if checkExpr(nospace) == true do 
       numList = String.split(nospace, ~r/\+|\-|\*|\//) 
       |> Enum.filter(fn num -> num != "" end)
+      |> Enum.map(fn num -> elem(Float.parse(num), 0) end)
       operatorList = String.split(nospace, ~r/\d/) 
       |> Enum.filter(fn operator -> operator != "" end)
       IO.puts(inspect(numList))
       IO.puts(inspect(operatorList))
-      Enum.reduce(numList, fn ele, acc -> [head | tail] = operatorList
-        evaluate(elem, acc, head) operatorList = tail end)
-  
+
+      [a | tail] = numList
+      recursive(tail, operatorList, a)
     else
       "Invalid expression"
     end
